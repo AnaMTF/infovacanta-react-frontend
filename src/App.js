036c8ce177ade54fa4f7bb5a99dc3e8e7f5d0a9e
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-05-23 14:22:15 +0300

    feat: Add new routes for registration and editing reviews

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Main } from "./pages/Main";
 import { Statiuni } from "./pages/Statiuni";
+import RegisterTemp from "./pages/RegisterTemp";
+import EditReview from "./pages/EditReviewTemp";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/main" element={<Main />} />
           <Route path="/statiuni" element={<Statiuni />} />
+          <Route path="/register" element={<RegisterTemp />} />
+          <Route path="/editreview" element={<EditReview />} />
         </Routes>
       </Router>
     </div>
