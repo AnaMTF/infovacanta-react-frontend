12b7bc0b3c88c0198e3ef6a04e7389d5a25fd7a9
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-06-12 21:35:12 +0300

    refactor: Update NewReview page with form submission and data fetching logic

--- src/pages/NewReview.js
+++ src/pages/NewReview.js
@@ -1,25 +1,67 @@
-import React, { Component } from 'react'
+import Axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+import { useState } from 'react'
+
+import { Link } from 'react-router-dom';
 
 import "../css/styles.css";
 import "../css/header.css";
 import "../css/new.css";
 
 export const NewReview = (props) => {
+    const [review_body, setReviewBody] = useState('');
+    const [destination_name, setDestinationName] = useState('');
+
+    const { data: destinations } = useQuery(["destination_id"], async function () {
+        try {
+            const response = await Axios.get(`http://localhost:5000/destinations`);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted");
+        console.log("Review body:", review_body);
+        console.log("Destination name:", destination_name);
+    };
+
     return (
         <div className="container jumbotron centered">
             <h1>Recenzie nouă</h1>
-            <form id="newPostForm" method="post" action="/reviews">
-                <input type="text" name="destinationname" placeholder="Destinația turistică vizitată" required />
+            <form id="newPostForm" onSubmit={handleSubmit}>
+                <input
+                    type="text" name="destination_name"
+                    placeholder="Destinația turistică vizitată"
+                    required
+                    value={destination_name}
+                    onChange={(e) => setDestinationName(e.target.value)}
+                    list="destination_names"
+                />
+                <datalist id="destination_names">
+                    {destinations?.map((destination, idx) => {
+                        return (
+                            <option key={idx} value={destination.destination_name} />
+                        );
+                    })}
+                </datalist>
                 <textarea
-                    name="reviewbody"
+                    name="review_body"
                     placeholder="Scrieți recenzia dumneavoastră aici..."
                     required
                     rows="10"
+                    value={review_body}
+                    onChange={(e) => setReviewBody(e.target.value)}
                 ></textarea>
                 <div id="starRating" data-lyket-type="rate" data-lyket-id="my-<%=starId%>-post" data-lyket-show-rating="user"></div>
                 <button className="full-width" type="submit" id="publicaBtn">Publică</button>
             </form>
-            <a className="full-width" href="/main" id="cancelBtn">Anulează</a>
+            <Link to="/main">
+                <button className="full-width" id="cancelBtn">Anulează</button>
+            </Link>
         </div>
     );
 };
