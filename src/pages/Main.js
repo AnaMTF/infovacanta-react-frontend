30ea9101c7225f2f12ec34b176f4e9574b715139
Anamaria <anamaria.titeche@gmail.com>
2024-07-09 22:50:04 +0300

    refactor: Add lazy loading for images in Register page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useSelector } from 'react-redux';
 import "../css/styles.css";
@@ -9,11 +9,18 @@ import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { Review } from '../components/Review';
 import { fetchAllReviews } from '../utils/fetchFunctions.js';
+import { useInView } from 'react-intersection-observer';
 
 export const Main = () => {
   const user = useSelector((state) => state.user.user);
 
   const { data: reviews } = useQuery(["Review Cards"], async () => fetchAllReviews());
+
+  const [visibleReviews, setVisibleReviews] = useState([]);
+  const [nextBatch, setNextBatch] = useState(0);
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
 
   useEffect(() => {
     const script1 = document.createElement('script');
@@ -31,6 +38,15 @@ export const Main = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (inView && nextBatch < reviews.length) {
+      const batchSize = 1; // Number of reviews to load per batch
+      const newBatch = reviews.slice(nextBatch, nextBatch + batchSize);
+      setVisibleReviews(prev => [...prev, ...newBatch]);
+      setNextBatch(nextBatch + batchSize);
+    }
+  }, [inView, nextBatch, reviews]);
+
   return (
     <div className="container-fluid jumbotron centered">
       <h1>InfoVacanță</h1>
@@ -39,10 +55,16 @@ export const Main = () => {
       </Link>
 
       <ul id="postsList" className="list-group">
-        {reviews?.map((review, idx) => {
+        {visibleReviews.map((review, idx) => {
           return (<Review key={idx} loggedInUserId={user?.user_id} content={review} />);
         })}
       </ul>
+
+      {nextBatch < reviews?.length && (
+        <div ref={ref}>
+          <h1>Se încarcă mai multe recenzii...</h1>
+        </div>
+      )}
     </div>
   );
 };
