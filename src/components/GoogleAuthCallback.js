9f1ca17efd094c4cba8952af1e41f02cb9069aad
johnny c. <56122426+pearmypie@users.noreply.github.com>
2024-07-09 15:02:30 +0300

    refactor: Add GoogleAuthCallback component and update routes in App.js and Login.js

--- src/components/GoogleAuthCallback.js
+++ src/components/GoogleAuthCallback.js
@@ -4,28 +4,28 @@ import { setUser } from '../app/userSlice';
 import { useEffect } from 'react';
 
 export const GoogleAuthCallback = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        console.log('location: ', location);
-        console.log('location.search: ', location.search);
+  useEffect(() => {
+    // console.log('location: ', location);
+    // console.log('location.search: ', location.search);
 
-        const query = new URLSearchParams(location.search);
-        console.log('query: ', query);
+    const query = new URLSearchParams(location.search);
+    // console.log('query: ', query);
 
-        const token = query.get('user');
-        console.log('query.get(\'user\') -> token: ', token);
+    const token = query.get('user');
+    // console.log('query.get(\'user\') -> token: ', token);
 
-        const user = JSON.parse(token);
-        console.log(user);
+    const user = JSON.parse(token);
+    // console.log(user);
 
-        if (user) {
-            dispatch(setUser(user));
-            navigate('/');
-        }
-    }, [dispatch, location.search, navigate]);
+    if (user) {
+      dispatch(setUser(user));
+      navigate('/');
+    }
+  }, [dispatch, location.search, navigate]);
 
-    return <p>Vă rugăm așteptați...</p>;
+  return <p>Vă rugăm așteptați...</p>;
 };
