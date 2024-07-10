e9be985cb44f0ee45fbaedaa36bff5d739ad8956
Anamaria <anamaria.titeche@gmail.com>
2024-07-10 20:17:28 +0300

    refactor: Optimize trigonometry functions and add comments for clarity

--- src/utils/trigonometryFunctions.js
+++ src/utils/trigonometryFunctions.js
@@ -1,27 +1,27 @@
 function degToRad(degrees) {
-    return degrees * (Math.PI / 180);
+  return degrees * (Math.PI / 180);
 }
 
 function radToDeg(rad) {
-    return rad / (Math.PI / 180);
+  return rad / (Math.PI / 180);
 }
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Radius of the earth in km
-    const dLat = degToRad(lat2 - lat1);
-    const dLon = degToRad(lon2 - lon1);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-        ;
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c; // Distance in km
+  const R = 6371; // Radius of the earth in km
+  const dLat = degToRad(lat2 - lat1);
+  const dLon = degToRad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
 }
 
 console.log(getDistanceFromLatLonInKm(
-    44.43434326443276, 26.165389641181687,
-    44.1766, 28.6150
+  44.43434326443276, 26.165389641181687,
+  44.1766, 28.6150
 ));
 
 export default getDistanceFromLatLonInKm;
